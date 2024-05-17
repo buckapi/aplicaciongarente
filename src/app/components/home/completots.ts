f12d911de109e536dc95607101c03e6cbd00f7ef
root <root@localhost.localdomain>
2024-05-17 04:57:51 -0500

    home

--- src/app/components/home/completots.ts
+++ src/app/components/home/completots.ts
@@ -6,6 +6,7 @@ import { DataApiService } from '../../services/data-api-service';
 import {
   FormBuilder,
   AbstractControl,
+  FormControlDirective,
   ReactiveFormsModule,
 } from '@angular/forms';
 import { FormGroup, Validators } from '@angular/forms';
@@ -15,12 +16,13 @@ import { FormGroup, Validators } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   ngFormRequest: FormGroup;
   submitted = false;
   public isError = false;
+
   constructor(
     public global: GlobalService,
     public yeoman: Yeoman,
@@ -62,26 +64,23 @@ export class HomeComponent {
       refEmailF2: ['', [Validators.required]]
     });
   }
+
   saveRequest() {
-    let data: any = {};
-    data = this.ngFormRequest.value;
-    /*   console.log("DATA: "+data.email)
-     */ // let request = { "name": data };
+    this.submitted = true;
+    if (this.ngFormRequest.invalid) {
+      return;
+    }
+
+    let data: any = this.ngFormRequest.value;
     this.dataApiService.saveRequest(data).subscribe(
       (response) => {
         console.log('Solicitud guardada correctamente:', response);
-        // Agregar la marca de la respuesta al array de marcas, si es necesario
-
-        // Limpiar los valores para futuros usos
         this.global.request = '';
         this.yeoman.allrequest.push(response);
         this.yeoman.allrequest = [...this.yeoman.allrequest];
         this.isError = false;
       },
       (error) => this.onIsError()
-      /*  error => {
-      console.error('Error al guardar :', error);
-    } */
     );
   }
 
@@ -119,18 +118,26 @@ export class HomeComponent {
       refCityF2: ['', [Validators.required]],
       refPhoneF2: ['', [Validators.required]],
       refEmailF2: ['', [Validators.required]]
-      
     });
   }
+
   get f(): { [key: string]: AbstractControl } {
     return this.ngFormRequest.controls;
   }
+
   onIsError(): void {
-    // this.ngxService.stop("loader-02");
     this.isError = true;
-    // this.ngxService.stop("loader-02");
     setTimeout(() => {
       this.isError = false;
     }, 4000);
+  }
+
+  nextSection() {
+    this.submitted = true;
+    if (this.ngFormRequest.invalid) {
+      return;
+    }
+
+    // Lógica para pasar a la siguiente sección
   }
 }
